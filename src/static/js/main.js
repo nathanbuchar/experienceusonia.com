fb6e2793b364145265c1ab6e3277013bc88cbf28
Nate M <hello@n8.engineer>
2025-02-17 08:11:55 -0500

    Update nav

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -2,24 +2,4 @@ function initLazyLoad() {
   lazyload();
 }
 
-function initNav() {
-  const navDetails = document.querySelector('.nav-module details');
-  const navScreen = navDetails.querySelector('.nav-module__screen')
-
-  navDetails.addEventListener('toggle', (evt) => {
-    if (navDetails.open) {
-      document.body.classList.add('noscroll');
-
-      navScreen.addEventListener('click', function onScreenClick() {
-        navDetails.removeAttribute('open');
-
-        document.body.removeEventListener('click', onScreenClick);
-      });
-    } else {
-      document.body.classList.remove('noscroll');
-    }
-  });
-}
-
 initLazyLoad();
-initNav();
