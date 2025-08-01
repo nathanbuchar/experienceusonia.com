cd08ca7401c2a25e0bea032dbf1d751cdcfcd04d
Nate M <hello@n8.engineer>
2025-08-01 01:58:43 -0400

    Caching and sublinks

---
+++ config.js
@@ -0,0 +1,75 @@
+import 'dotenv/config';
+
+import Builder from './lib/builder.js';
+
+import client from './lib/client.js';
+import render from './lib/render.js';
+
+import cache from './lib/plugins/cache.js';
+import clean from './lib/plugins/clean.js';
+import contentful from './lib/plugins/contentful.js';
+import copy from './lib/plugins/copy.js';
+import tickets from './lib/plugins/tickets.js';
+
+Builder.build({
+  render,
+  plugins: [
+    clean('dist'),
+    cache({
+      disabled: process.argv.includes('--no-cache'),
+      plugins: [
+        contentful({
+          client,
+          sources: [
+            {
+              key: 'pages',
+              contentType: 'page',
+            },
+            {
+              key: 'navLinks',
+              contentType: 'navLink',
+            },
+            {
+              key: 'opengraph',
+              contentType: 'opengraph',
+            },
+          ],
+        }),
+        tickets(),
+      ],
+    }),
+    copy({
+      from: 'src/static',
+      to: 'dist',
+    }),
+  ],
+  targets: [
+    {
+      template: '404.njk',
+      dest: 'dist/404.html',
+      include: ['navLinks', 'opengraph'],
+    },
+    {
+      template: 'test.njk',
+      dest: 'dist/test/index.html',
+      include: ['navLinks', 'opengraph'],
+    },
+    {
+      template: 'debug.njk',
+      dest: 'dist/debug/index.html',
+      include: '*',
+    },
+    (ctx) => {
+      return ctx.pages.map((page) => {
+        return {
+          template: 'page.njk',
+          dest: `dist/${page.fields.url}/index.html`,
+          include: '*',
+          extraContext: {
+            ...page.fields,
+          },
+        };
+      });
+    },
+  ],
+});
